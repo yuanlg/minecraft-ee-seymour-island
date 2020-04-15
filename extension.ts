d0080eeea5da076441e1dc7b3dbc7210ddbbd23d
Andrew Mulholland <gbaman@users.noreply.github.com>
2020-04-15 15:12:44 +0100

    Add placeFloor

--- extension.ts
+++ extension.ts
@@ -25,5 +25,32 @@ namespace seymour {
     export function placeRedstoneBelow() {
         agent.place(DOWN)
     }
+   
+   /**
+     * Places one of the available floor blocks below
+     */
+    //% block="agent place floor below"
+    //% weight=90
+   export function placeFloor (blockid: number) {
+      // 0 - 41 - Gold
+      // 1 - 133 - Emerald
+      // 2 - 57 - Gold
+      // 3 - 22 - Lapis
+      // 4 - 152 - Redstone
+    if (blockid == 41) {
+        agent.setSlot(0)
+    } else if (blockid == 133) {
+        agent.setSlot(1)
+    } else if (blockid == 57) {
+        agent.setSlot(2)
+    } else if (blockid == 22) {
+        agent.setSlot(3)
+    } else if (blockid == 152) {
+        agent.setSlot(4)
+    } else {
+        agent.setSlot(26)
+    }
+    agent.place(DOWN)
+}
 
 }
